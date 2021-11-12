99ca20f54aa4b3080d560fdf250aaea71402891d
agusssli <agustinlibon@gmail.com>
2021-11-12 08:13:22 -0300

    primera prueba de brblod page

---
+++ php/registro.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once "conection.php";
+$asunto=$_POST['asunto'];
+$email=$_POST['email'];
+$texto=$_POST['texto'];
+
+$obj = new connection();
+$con = $obj->connect();
+$intruccion_sql = "INSERT INTO contacto(asunto, email, texto)
+values ('($asunto)', '($email)', '($texto)')";
+$result = $con->prepare($instruccion_sql);
+$result->execute();
+
+
+
+
+
+
+
+
+?>
